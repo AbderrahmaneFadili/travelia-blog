a38b7b35d7dcbee33ea1e566413fbe612cf5fbbb
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-11-22 14:23:12 +0100

    Create the Footer Component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <span>Footer </span>
+    <footer className="bg-gray-900 text-white p-20">
+      <p className="text-center">
+        <span className="text-2xl mr-4">Travelia</span>
+        <span>Copyright &copy; {new Date().getFullYear()}</span>
+      </p>
     </footer>
   );
 };
