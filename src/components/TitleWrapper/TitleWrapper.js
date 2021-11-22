3c0a984cfcf34a9e4aafba172245350f6d7e4bad
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-11-22 14:49:15 +0100

    Title Wrapper Component

---
+++ src/components/TitleWrapper/TitleWrapper.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TitleWrapper = ({ children }) => {
+  return (
+    <header className="bg-gray-900 flex flex-col items-center justify-center text-white py-20 px-16">
+      <div className="max-w-screen-md">{children}</div>
+    </header>
+  );
+};
+
+export default TitleWrapper;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,51 @@
 import React, { Component } from "react";
+import TitleWrapper from "../components/TitleWrapper/TitleWrapper";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        {
+          id: 1,
+          title: "Air travel",
+        },
+        {
+          id: 2,
+          title: "Bleisure travel",
+        },
+        {
+          id: 3,
+          title: "Honeymoon",
+        },
+      ],
+    };
+  }
+
   render() {
+    const { categories } = this.state;
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <>
+        {/* Title Wrapper */}
+        <TitleWrapper>
+          <h1 className="text-8xl text-center font-light">
+            Welcome to Travelia
+          </h1>
+
+          <h3 className="font-extralight text-2xl text-gray-400 my-8 text-center">
+            Blog Categories
+          </h3>
+          <ul className="flex w-72 justify-between mx-auto">
+            {categories.map((c) => (
+              <li key={c.id}>
+                <button className="cursor-pointer text-xl">{c.title}</button>
+              </li>
+            ))}
+          </ul>
+        </TitleWrapper>
+        {/* Search */}
+        {/* Blog Posts  */}
+      </>
     );
   }
 }
